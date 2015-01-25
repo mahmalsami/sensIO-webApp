e2dd7b9e0206c6334a637b2781f998e31a4bfdcc
Samix <reaverhack@gmail.com>
2015-01-25 14:31:43 +0100

    Update

--- server/api/api.js
+++ server/api/api.js
@@ -3,75 +3,117 @@
 
 exports.savePacket = function(req, res) {
 
-	var db = require('../config/database').connect();
+	//var db = require('../config/database').connect();
 	
-
-
-
-//Sample to test MYSQL
-// 	db.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
-
-//   console.log('The solution is: ', rows[0].solution);
-
 
 
 	var nodeID = parseInt(req.body.nodeID);
 	var sequenceNumber = parseInt(req.body.seq);
-	var delay = parseInt(req.body.dur);
-	var nodeIDAuthor = parseInt(req.body.nodeIDAuthor);
+	var duration = parseInt(req.body.dur);
+	var sourceId = parseInt(req.body.sourceId);
 
 	var now= new Date();
-
-
-	db.collection('nodes').find({'nodeID':nodeID,'sequenceNumber':sequenceNumber}).toArray(function(err, result) {
-
-		if(result.length == 0){
-		  db.collection('nodes').insert({'nodeID':nodeID,'sequenceNumber':sequenceNumber,'delay':delay,'publicationDate':now.toISOString().substr(0,10)}, function(err) {
-		    if (err) {
-		      throw err;
-					res.status(500).json(err);
-		    }else {
-		      res.status(201).json({'response':"Sucessfully Registered"});
-		    }
-		  });
-
-		}else{
-			res.status(201).json({'response':"Already registred"});
-		}
-	 });
+	var publicationDate = now.toISOString().substr(0,10);
+	var hour = parseInt(now.toISOString().substr(11,6));
 
 
 
-		db.collection('node-list').find({'nodeID':nodeID}).toArray(function(err, result) {
-			if(result == 0){
+if(duration!=0){
 
-			  db.collection('node-list').insert({'nodeID':nodeID}, function(err) {
-			    if (err) {
-			      throw err;
-						conole.log(err);
-			    }else {
-			      console.log("Sucessfully Added to list");
-			    }
-			  });
-			}
+	var mysql      = require('mysql');
+
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
+	});
+
+	connection.connect();
+
+	//Sample to test MYSQL
+	//var selectQuery = '"SELECT COUNT(*) FROM SENSIO WHERE nodeID='+nodeID+' AND sequenceNumber='+sequenceNumber+'"';
+	var selectQuery = 'SELECT COUNT(*) FROM SENSIO WHERE nodeID='+nodeID+' AND sequenceNumber='+sequenceNumber;
+	console.log(selectQuery);
+	var queryResult = 0;
+	connection.query(selectQuery, function(err, rows, fields) {
+	  if (err) {
+	  	connection.end();
+	  	throw err;
+	  }
+
+	  queryResult = parseInt(rows[0]['COUNT(*)']);
+	});
+
+
+	//To be done later
+	console.log('query result '+queryResult);
+	if(!queryResult){
+		var insertQuery = 'INSERT INTO SENSIO VALUES('+nodeID+','+duration+','+publicationDate+','+hour+','+sequenceNumber+','+sourceId+')';
+
+		connection.query(insertQuery.toString(), function(err, rows, fields) {
+		 if (err) {
+	  	throw err;
+	  	connection.end();
+	  }
 		});
+	}
+
+
+	connection.end();
+}
+
+res.status(201).json({'response':"OK"});
+
+
+	// db.collection('nodes').find({'nodeID':nodeID,'sequenceNumber':sequenceNumber}).toArray(function(err, result) {
+
+	// 	if(result.length == 0){
+	// 	  db.collection('nodes').insert({'nodeID':nodeID,'sequenceNumber':sequenceNumber,'delay':delay,'publicationDate':now.toISOString().substr(0,10)}, function(err) {
+	// 	    if (err) {
+	// 	      throw err;
+	// 				res.status(500).json(err);
+	// 	    }else {
+	// 	      res.status(201).json({'response':"Sucessfully Registered"});
+	// 	    }
+	// 	  });
+
+	// 	}else{
+	// 		res.status(201).json({'response':"Already registred"});
+	// 	}
+	//  });
 
 
 
-		db.collection('node-list-connected').find({'nodeID':nodeIDAuthor}).toArray(function(err, result) {
-			if(result == 0){
+		// db.collection('node-list').find({'nodeID':nodeID}).toArray(function(err, result) {
+		// 	if(result == 0){
 
-			  db.collection('node-list-connected').insert({'nodeID':nodeIDAuthor}, function(err) {
-			    if (err) {
-			      throw err;
-						conole.log(err);
-			    }else {
-			      console.log("Sucessfully Added to list");
-			    }
-			  });
-			}
-		});
+		// 	  db.collection('node-list').insert({'nodeID':nodeID}, function(err) {
+		// 	    if (err) {
+		// 	      throw err;
+		// 				conole.log(err);
+		// 	    }else {
+		// 	      console.log("Sucessfully Added to list");
+		// 	    }
+		// 	  });
+		// 	}
+		// });
+
+
+
+		// db.collection('node-list-connected').find({'nodeID':nodeIDAuthor}).toArray(function(err, result) {
+		// 	if(result == 0){
+
+		// 	  db.collection('node-list-connected').insert({'nodeID':nodeIDAuthor}, function(err) {
+		// 	    if (err) {
+		// 	      throw err;
+		// 				conole.log(err);
+		// 	    }else {
+		// 	      console.log("Sucessfully Added to list");
+		// 	    }
+		// 	  });
+		// 	}
+		// });
 
 
 
@@ -108,22 +150,44 @@ exports.awesomeThings = function(req, res) {
 
 exports.currentWeekTendancy = function(req, res) {
 
-	var db = require('../config/database').connect();
+	// var db = require('../config/database').connect();
+var result = ['a','b'];
+	// var now= new Date();
 
-	var now= new Date();
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
+	});
 
-	db.collection('nodes').find({'publicationDate':now.toISOString().substr(0,10)}).toArray(function(err, result) {
-	  res.json([
+	connection.connect();
+
+	var insertQuery = 'SELECT SUM(duration) FROM SENSIO';
+
+	connection.query(insertQuery.toString(), function(err, rows, fields) {
+	  if (err) {
+	  	throw err;
+	  	connection.end();
+	  }
+	  	 res.json([
 	    { Day: 'Mon.', value: 0 },
 	    { Day: 'Tue.', value: 0 },
 	    { Day: 'Wed.', value: 0 },
 	    { Day: 'Thur', value: 0 },
 	    { Day: 'Frid.', value: 0 },
 	    { Day: 'Sat.', value: 10 },
-	    { Day: 'Sund.', value: result.length }
+	    { Day: 'Sund.', value: parseInt(rows[0]['SUM(duration)']/1000)}
 	  ]);
+	});
 
-	 });
+	connection.end();
+
+	// db.collection('nodes').find({'publicationDate':now.toISOString().substr(0,10)}).toArray(function(err, result) {
+
+
+	 // });
 
 
 };
@@ -131,9 +195,27 @@ exports.currentWeekTendancy = function(req, res) {
 
 
 exports.globalTendance = function(req, res) {
+	var result = ['a','b'];
+	// var db = require('../config/database').connect();
+
+
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
+	});
+
+	connection.connect();
 	
-	var db = require('../config/database').connect();
-	db.collection('nodes').find().toArray(function(err, result) {
+	var insertQuery = 'SELECT SUM(duration) FROM SENSIO';
+
+	connection.query(insertQuery.toString(), function(err, rows, fields) {
+		if (err) {
+	  	throw err;
+	  	connection.end();
+	  }
 	  res.json([
 	    { y: 'June', a: 0,  b: 10 },
 	    { y: 'July', a: 0,  b: 11 },
@@ -142,23 +224,84 @@ exports.globalTendance = function(req, res) {
 	    { y: 'Oct.', a: 0,  b: 13 },
 	    { y: 'Nov.', a: 0,  b: 10 },
 	    { y: 'Dec.', a: 0, b: 11 },
-	    { y: 'Janu.', a: result.length, b: 12 }
+	    { y: 'Janu.', a:parseInt(rows[0]['SUM(duration)']/1000), b: 12 }
 	  ]);
+	    connection.end();
   });
 };
 
 
 
+
+
 exports.currentNodeState = function(req, res) {
-  res.json([
-    { Day: '8', a: 0, b: 0 },
-    { Day: '10', a: 0, b: 0 },
-    { Day: '12', a: 0, b: 0 },
-    { Day: '14', a: 0, b: 0 },
-    { Day: '16', a: 0, b: 0 },
-    { Day: '18', a: 1, b: 10 },
-    { Day: '20', a: 0, b: 0 }
-  ]);
+
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
+	});
+	
+	connection.connect();
+
+	var insertQuery = 'SELECT nodeID,hour,SUM(duration) FROM SENSIO GROUP BY nodeID,hour';//WHERE nodeID=1 OR nodeID=2 GROUPBY nodeID,hour
+//var insertQuery = 'SELECT * FROM SENSIO';
+
+connection.query(insertQuery.toString(), function(err, rows, fields) {
+	if (err) {
+	  	throw err;
+	  	connection.end();
+	}
+
+	var inter =0;
+	//var tab = [0,1,2,3,4,5,6];
+	var tabA = new Array(22);
+	var tabB = new Array(22);
+
+	for(i=0;i<tabA.length;i++){
+		tabA[i]=0;
+		tabB[i]=0;
+	}
+
+	var i;
+	
+
+	for(i=0;i<rows.length;i++){
+
+		if(rows[i]['nodeID']==1){
+			inter = rows[i]['hour'];
+			tabA[inter-(inter % 2)] += rows[i]['SUM(duration)'];
+		}
+
+		if(rows[i]['nodeID']==2){
+			inter = rows[i]['hour'];
+			tabB[inter-(inter % 2)] += rows[i]['SUM(duration)'];
+		}
+
+		console.log(rows[i]['nodeID']);
+	}	
+
+	for(i=0;i<tabA.length;i++){
+		console.log('tab value' + tabA[i]);
+		console.log('tab value' + tabB[i]);
+	}
+
+	console.log(JSON.stringify(rows)+'  reallly ???');
+	  res.json([
+	    { Day:  '8', a: tabA[8]/1000,  b: tabB[8]/1000 },
+	    { Day: '10', a: tabA[10]/1000, b: tabB[10]/1000 },
+	    { Day: '12', a: tabA[12]/1000, b: tabB[12]/1000 },
+	    { Day: '14', a: tabA[14]/1000, b:tabB[14]/1000 },
+	    { Day: '16', a: tabA[16]/1000, b:tabB[16]/1000 },
+	    { Day: '18', a: tabA[18]/1000, b:tabB[18]/1000},
+	    { Day: '20', a: tabA[20]/1000, b:tabB[20]/1000 }
+	  ]);
+	    connection.end();
+});
+
+
 };
 
 
@@ -167,19 +310,51 @@ exports.currentNodeState = function(req, res) {
 
 exports.todayTendance = function(req, res) {
 
-	var db = require('../config/database').connect();
 
-	db.collection('nodes').find({'nodeID':1}).toArray(function(err, result) {
-		var previousResult = result.length;
-
-		db.collection('nodes').find({'nodeID':2}).toArray(function(err, result) {
-		  res.json([
-		    {label: 'SensIO Stand A', value: previousResult},
-		    {label: 'SensIO Stand B', value: result.length}
-		  ]);
-		});
-
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
 	});
+
+	connection.connect();
+
+	var insertQuery = 'SELECT SUM(duration),nodeID FROM SENSIO WHERE nodeID=1 OR nodeID=2';
+	// var db = require('../config/database').connect();
+	
+	connection.query(insertQuery.toString(), function(err, rows, fields) {
+		if (err) {
+	  	throw err;
+	  	connection.end();
+	  }
+	// db.collection('nodes').find({'nodeID':1}).toArray(function(err, result) {
+		// var result = ['a','b'];
+		// var previousResult = result.length;
+		// var result = ['a','b'];
+		var valueA =0;
+		var valueB =0;
+
+		var i;
+		for(i=0;i<rows.length;i++){
+			if(rows[i]['nodeID']==1){
+				valueA=rows[i]['SUM(duration)'];
+			}else{
+				valueB=rows[i]['SUM(duration)'];
+			}
+		}
+		// db.collection('nodes').find({'nodeID':2}).toArray(function(err, result) {
+		  res.json([
+		    {label: 'SensIO Stand A', value: valueA},
+		    {label: 'SensIO Stand B', value: valueB}
+		  ]);
+		  connection.end();
+	});
+
+		// });
+
+	// });
 
 };
 
@@ -189,7 +364,7 @@ exports.todayTendance = function(req, res) {
 
 
 exports.maintenanceState = function(req, res) {
-	var db = require('../config/database').connect();
+	// var db = require('../config/database').connect();
 
 	var totalFleet=0;
 	var totalConnected=0;
@@ -201,18 +376,18 @@ exports.maintenanceState = function(req, res) {
 
 
 
-	db.collection('node-list').find().toArray(function(err, result) {
-
+	// db.collection('node-list').find().toArray(function(err, result) {
+		var result = ['a','b'];
     totalFleet = result.length;
 
 
-    db.collection('node-list-connected').find().toArray(function(err, result) {
+    // db.collection('node-list-connected').find().toArray(function(err, result) {
     	totalConnected = result.length;
 
 		  res.json(
 		    {
-		      totalFleet: totalFleet,
-		      connectedNodes: totalConnected,
+		      totalFleet: 3,
+		      connectedNodes: 1,
 		      warnings: 0,
 		      alerts: 0,
 		      logs:[
@@ -231,8 +406,8 @@ exports.maintenanceState = function(req, res) {
 		      ]
 		    }
 		  );
-		});//end of find node-list-connected
+	// 	});//end of find node-list-connected
 
-	});//end of find node-list
+	// });//end of find node-list
 
 };
--- server/config/database.js
+++ server/config/database.js
@@ -2,21 +2,22 @@
 
 function connect(){
 
-var db = require('mongoskin').db('mongodb://barber-user:barber-pass00@ds027771.mongolab.com:27771/barber', {safe:true});
+// var db = require('mongoskin').db('mongodb://barber-user:barber-pass00@ds027771.mongolab.com:27771/barber', {safe:true});
 
-	console.log("[database.js]");
-	return db;
-
-//For MYSQL IMPLEMENTATION
-	// var mysql      = require('mysql');
-	// var connection = mysql.createConnection({
-	//   host     : 'localhost',
-	//   user     : 'me',
-	//   password : 'secret'
-	// });
+// 	console.log("[database.js]");
+// 	return db;
 
 
-	// return connection.connect();
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+	  host     : 'us-cdbr-azure-southcentral-e.cloudapp.net',
+	  user     : 'b81ebb2b0c105f',
+	  password : '1a1f8033',
+	  database : 'sensIO6AVa02ksnb'
+	});
+
+
+	return connection.connect();
 }
 
 exports.connect=connect;
